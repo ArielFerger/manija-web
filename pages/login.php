6eff7a04d726baf2f771886f2cb657656efcef96
Franc0-L <francoleonettu123@gmail.com>
2026-09-03 18:35:53 -0300

    Mis cambios

--- pages/login.php
+++ pages/login.php
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-<?php echo" 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN</title>
-</head>
-<body>
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</body>
-</html>
-"
-?> 
-=======
 <?php include("../php/includesBasics/header.php"); ?>
 
 <div class="container mt-5">
@@ -127,4 +96,3 @@
 <script src="../js/validacionesForm.js"></script>
 
 <?php include("../php/includesBasics/footer.php"); ?>
->>>>>>> 35ffa5b65b35205ce0120ae76bd9bc6b47e1f9c7
